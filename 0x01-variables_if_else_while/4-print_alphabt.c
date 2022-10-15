749de1655590b9de6e1cb4dbb5ba51c6580c8969
olyadadugna <olyada.tafa>
2022-10-14 18:18:11 -0700

    olikujl

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,20 @@
 #include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if (( c != 'q' && c != 'e') && с <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
