c1f3f420b4e28a535df4600356c8f0163e0bf2cd
olyadadugna <olyada.tafa>
2022-10-17 18:50:58 -0700

    olii

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,15 @@ int main(void)
 	 int ones = '0';
 	 int tens = '0';
 
-	 for (tens= "0"; tens <= '9'; tens++)
+	 for (tens = "0"; tens <= '9'; tens++)
 	 {
-		 for (ones ='0'; ones <= '9'; ones++)
+		 for (ones = '0'; ones <= '9'; ones++)
 		 {
 			 if (!((ones == tens) || (tens > ones)))
 			 {
 				 putchar(tens);
 				 putchar(ones);
-				 if (1(ones == '9' && tens == '8'))
+				 if (!(ones == '9' && tens == '8'))
 				 {
 					 putchar(',');
 					 putchar(' ');
