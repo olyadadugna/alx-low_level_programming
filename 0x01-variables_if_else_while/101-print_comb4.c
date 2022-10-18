0bdacbfd34d8ec44cd21965c2d5192f8fd3d69a4
olyadadugna <olyada.tafa@gmail.com>
2022-10-18 05:51:45 +0300

    olit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,18 +6,28 @@
  * */
 int main(void)
 {
-	int i, j, k;
+        int i, j, k;
 
-	for (i = '0'; i <= '9'; i++)
-	{
-		for (j = '0'; j <= '9'; j++)
-		{
-			for (k = '0'; k <= '9'; k++)
-			{
-				if (i < j && j < k)
+        for (i = '0'; i <= '9'; i++)
+        {
+                for (j = '0'; j <= '9'; j++)
+                {
+                        for (k = '0'; k <= '9'; k++)
+                        {
+                                if (i < j && j < k)
+                                {
+                                        putchar(i);
+                                        putchar(j);
+					putchar(k);
+				if (i != '7')
 				{
-					putchar(i);
-					putchar(j);
-				utchar(k);
-					{
-					
+					putchar(',');
+					putchar(' ');
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
