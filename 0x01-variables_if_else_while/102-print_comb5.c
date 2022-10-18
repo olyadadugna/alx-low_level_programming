2a52729105edaf8c9874286de694e9f76a48d1d1
olyadadugna <olyada.tafa@gmail.com>
2022-10-18 05:56:55 +0300

    olitp

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * main - print two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			if (a < 98)
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
