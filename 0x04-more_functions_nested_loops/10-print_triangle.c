a9e44a55cbde21efbca18cb52c8a28c261faf040
root <root@LAPTOP-0MBVDSHJ>
2022-10-22 01:46:25 +0300

    oli

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @i: size of the triangle
+ */
+void print_triangle(int i)
+{
+	int j, k;
+
+	if (i > 0)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			for (k = i - j; k > 0; k--)
+				_putchar(' ')
+		
+			for (k = 0; k < j; k--)
+				_putchar('#');
+
+			if (j == i)
+			       continue;
+			
+			_putchar('\n');
+		}
+	}
+	
+	_putchar('\n');
+}	
