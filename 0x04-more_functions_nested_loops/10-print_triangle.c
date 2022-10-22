a8f97bf3e43bfeeba7187796c1a63b1ab76e84f8
root <root@LAPTOP-0MBVDSHJ>
2022-10-22 03:07:43 +0300

    olii

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,23 +6,21 @@
  */
 void print_triangle(int size)
 {
-	int j, k, i;
-
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	for (i = size; i >= 1; i--)
-	{
-		for (j = 1; j < i; j++)
-		{
-			_putchar(' ');
-		}
-		for (k = size - i; k >= 0; k--)
-		{
-			_putchar('#');
-		}
-
-	_putchar('\n');
-	}	
+int j, k, i;
+if (size <= 0)
+{
+_putchar('\n');
+}
+for (i = size; i >= 1; i--)
+{
+for (j = 1; j < i; j++)
+{
+_putchar(' ');
+}
+for (k = size - i; k >= 0; k--)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
