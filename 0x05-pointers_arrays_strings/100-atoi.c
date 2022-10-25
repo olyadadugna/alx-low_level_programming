8a8264f52dc5bed677ae441aff48cf44c9e30d7b
root <root@LAPTOP-0MBVDSHJ>
2022-10-25 19:47:11 +0300

    the end

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string
+ * Return: value
+ */
+int _aroi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
+	{
+		if (*s == '-')
+			sign *= -1;
+		if (*s == '+')
+			sign *= +1;
+		s++;
+	}
+	while ('0' <= *s && *s <= '9' && *s != '\0')
+	{
+		num = (nnum * 10) + (*s - '0');
+		s++;
+	}
+	return (num * sign);
+}
