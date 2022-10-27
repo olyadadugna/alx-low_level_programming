a51c8136b6158ebef6bc4700a667b8d79813b7d8
root <root@LAPTOP-0MBVDSHJ>
2022-10-28 00:22:04 +0300

    end3

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
 /**
  * reverse_array - reverses an array of integers
  * @a: array
- * @n: number of elements of array
+ * @n: number of elements of the array
  * Return: rev
  */
 void reverse_array(int *a, int n)
