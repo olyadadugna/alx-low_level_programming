d66261921643f852947fd3404f99a9fb0782be32
root <root@LAPTOP-0MBVDSHJ>
2022-10-28 00:09:53 +0300

    end1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,32 +2,32 @@
 
 /**
  * cap_string - capitalizes words
- * @strr: string
+ * @str: string
  * Return: pointer to string
  */
 char *cap_string(char *str)
 {
-	int m =0;
+	int m = 0;
 
 	while (str[m])
 	{
 		while (!(str[m] >= 'a' && str[m] <= 'z'))
 			m++;
-		if (str[m -1] == ' ' ||
-				str[m -1] == ' ' ||
-				str[m -1] == '\t' ||
-				str[m -1] == '\n' ||
-				str[m -1] == ',' ||
-				str[m -1] == ';' ||
-				str[m -1] == '.' ||
-				str[m -1] == '!' ||
-				str[m -1] == '?' ||
-				str[m -1] == '"' ||
-				str[m -1] == '(' ||
-				str[m -1] == ')' ||
-				str[m -1] == '{' ||
-				str[m -1] == '}' ||
-				i == 0)
+		if (str[m - 1] == ' ' ||
+				str[m - 1] == ' ' ||
+				str[m - 1] == '\t' ||
+				str[m - 1] == '\n' ||
+				str[m - 1] == ',' ||
+				str[m - 1] == ';' ||
+				str[m - 1] == '.' ||
+				str[m - 1] == '!' ||
+				str[m - 1] == '?' ||
+				str[m - 1] == '"' ||
+				str[m - 1] == '(' ||
+				str[m - 1] == ')' ||
+				str[m - 1] == '{' ||
+				str[m - 1] == '}' ||
+				m == 0)
 			str[m] -= 32;
 		m++;
 	}
