82b5387096d67e4ae6ab9832359e122f631050c1
root <root@LAPTOP-0MBVDSHJ>
2022-10-28 00:02:33 +0300

    end

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,27 @@
-#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strncpy - copys a string with n
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	a = 0;
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,21 @@
-#include <stdlib.h>
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a: array
+ * @n: numb of elements of array
+ * Return: rev
+ */
+void reverse_array(int *a, int n)
+{
+	int c;
+	int b;
+
+	for (c = 0; c < n; c++)
+	{
+		n--;
+		b = a[c];
+		a[c] = a[n];
+		a[n] = b;
+	}
+}	
