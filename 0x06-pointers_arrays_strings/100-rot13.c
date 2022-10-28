15b97a69b00befb640c265f1dcc289af800e195c
root <root@LAPTOP-0MBVDSHJ>
2022-10-28 04:02:52 +0300

    zzz

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
- * *rot13 - function that encodes string using rotl3
- * @s: string
- * Return: encoded string
+ * *rot13 - Encodes a string using rotl3
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to the encoded string
  */
-char *rot13(char *)
+char *rot13(char *str)
 {
-	int indxl = 0, indx2;
+	int indx1 = 0, indx2;
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
