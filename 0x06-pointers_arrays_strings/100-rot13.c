cc2ea25c4fad5efa2a3630a1d3536630eaeca9c9
root <root@LAPTOP-0MBVDSHJ>
2022-10-28 03:09:35 +0300

    zzzz

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,26 @@
-#include <stdlib.h>
+#include "main.h"
+/**
+ * *rot13 - function that encodes string using rotl3
+ * @s: string
+ * Return: encoded string
+ */
+char *rot13(char *)
+{
+	int x, y;
+
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; input[y] != '\0'; y++)
+		{
+			if (s[x] == input[y])
+			{
+				s[x] = output[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
