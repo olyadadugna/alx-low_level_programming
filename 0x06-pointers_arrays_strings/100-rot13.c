df4e60a833c618966da8750fc7e757dda508e15a
root <root@LAPTOP-0MBVDSHJ>
2022-10-28 03:54:50 +0300

    zzzm

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,21 +6,21 @@
  */
 char *rot13(char *)
 {
-	int x, y;
+	int indxl = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
-	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (x = 0; s[x] != '\0'; x++)
+	for (str[indx1])
 	{
-		for (y = 0; input[y] != '\0'; y++)
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-			if (s[x] == input[y])
+			if (str[indx1] == alphabet[indx2])
 			{
-				s[x] = output[y];
+				str[indx1] = rot13key[indx2];
 				break;
 			}
 		}
+		indx1++;
 	}
-	return (s);
+	return (str);
 }
