09d8b9a939f654bb6a7aecf9157451d41a2462b1
root <root@LAPTOP-0MBVDSHJ>
2022-10-28 04:27:54 +0300

    zzmmsm

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *rot13 - Encodes a string using rotl3
+ * rot13 - Encodes a string using rot13
  * @str: The string to be encoded
  *
  * Return: A pointer to the encoded string
@@ -22,7 +22,9 @@ char *rot13(char *str)
 				break;
 			}
 		}
+
 		indx1++;
 	}
+
 	return (str);
 }
