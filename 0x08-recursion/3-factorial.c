edf2835e15490c6041de61b288c9901a6edd664e
root <root@LAPTOP-0MBVDSHJ>
2022-11-03 00:33:26 +0300

    hot fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,8 +4,7 @@
  * factorial - Returns the factorial of a given numb
  * @n: the number to the factorial
  *
- * Return: if n > 0 - the factorial of n
- * 	if n < 0 - 1 to indicate an error
+ * Return: an integer
  */
 int factorial(int n)
 {
@@ -13,7 +12,7 @@ int factorial(int n)
 
 	if (n < 0)
 		return (-1);
-
-	else if (n >= 0 && n <= 1)
+	if (n <= 1)
 		return (1);
+	return (n * factorial(n - 1));
 }
