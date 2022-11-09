8aeb7a135d7a1e9dee6ab376d8646813d6e948f6
root <root@LAPTOP-0MBVDSHJ>
2022-11-09 14:34:57 +0300

    oli

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,12 @@
  * @n: The number of bytes to be filled
  * Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)i
 {
 	unsigned int index;
-	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
-		memory[index] = value;
+		s[index] = b;
 
-	return (memory);
+	return (s);
 }
