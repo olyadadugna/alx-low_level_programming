31523da2ac824b7e3b691dd9ecb361942820e010
root <root@LAPTOP-0MBVDSHJ>
2022-12-05 11:53:11 +0300

    oMK

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,25 +1,17 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees a listint_t list.
- * @head: A pointer to the address of the
- *       head of the listint_t list.
- *
- * Description: Sets the head to NULL.
+ ** free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the listint_t list to be freed.
  */
-void free_listint2(listint_t **head)
+void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-	if (head == NULL)
-		return;
-
-	while (*head)
+	while (head)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
-
-	head = NULL;
 }
